2b1f3ccde0ae3854b72d1a17fd25ffd34648dc33
TianAoyang <tianaoyang@pimpin.cn>
2016-06-26 17:47:59 +0800

    Aaron Tian: Change the code

--- run_analysis.R
+++ run_analysis.R
@@ -4,43 +4,35 @@ train.y <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 test.y <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
 Subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 Subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
-activity_lables<-read.table("./data/UCI HAR Dataset/activity_labels.txt")
+activity_lables <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 features<-read.table("./data/UCI HAR Dataset/features.txt")
 
-all.x <- rbind(train.x, test.x)
+all.x <- rbind(train.x,test.x)
+colnames(all.x) <- c(as.character(features[,2]))
 
-colnames(all.x) <- c(as.character(features[, 2]))
+Mean <- grep("mean()",colnames(all.x),fixed=TRUE)
 
-Mean <- grep("mean()", colnames(all.x), fixed = TRUE)
+SD <- grep("std()",colnames(all.x),fixed=TRUE)
 
-SD <- grep("std()", colnames(all.x), fixed = TRUE)
+MeanSD <- all.x[,c(Mean,SD)]
 
-MeanSD <- all.x[, c(Mean, SD)]
+all.y <- rbind(train.y,test.y)
 
-all.y <- rbind(train.y, test.y)
-
-all.activity <- cbind(all.y, MeanSD)
+all.activity <- cbind(all.y,MeanSD)
 
 colnames(all.activity)[1] <- "Activity"
 
-activity_lables[, 2] <- as.character(activity_lables[, 2])
-
-for (i in 1:length(all.activity[, 1])) {
-    all.activity[i, 1] <- activity_lables[all.activity[i,1], 2]
+activity_lables[,2] <- as.character(activity_lables[,2])
+for(i in 1:length(all.activity[,1])){
+  all.activity[i,1] <- activity_lables[all.activity[i,1],2]
 }
-
-Subject_all <- rbind(Subject_train, Subject_test)
-
-all <- cbind(Subject_all, all.activity)
-
+Subject_all <- rbind(Subject_train,Subject_test)
+all <- cbind(Subject_all,all.activity)
 colnames(all)[1] <- "Subject"
-
-Tidy <- aggregate(all[, 3] ~ Subject + Activity, data = all, FUN = "mean")
-
+Tidy <- aggregate( all[,3] ~ Subject+Activity, data = all, FUN= "mean" )
 for(i in 4:ncol(all)){
-    Tidy[, i] <- aggregate(all[, i] ~ Subject + Activity, data = all, FUN = "mean")
+  Tidy[,i] <- aggregate( all[,i] ~ Subject+Activity, data = all, FUN= "mean" )[,3]
 }
-
-colnames(Tidy)[3 : ncol(Tidy)] <- colnames(MeanSD)
+colnames(Tidy)[3:ncol(Tidy)] <- colnames(MeanSD)
 
 write.table(Tidy, file = "FinalData.txt")
